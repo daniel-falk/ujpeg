ab726b48c55410924e42d54907b3e44f82c5b138
Daniel Falk <daniel@da-robotteknik.se>
2020-05-24 17:50:21 +0200

    Add test image and improve test code

--- test.py
+++ test.py
@@ -1,24 +1,55 @@
+"""
+We should include the micropython version of pytest to write real
+unit tests. For now, lets just try the most important behaviour.
+"""
+
 import ujpeg
 
-ujpeg.decode_file("test.jpg", "test.pgm")
-
 try:
-    ujpeg.decode_file("/does-not-exist.jpg", "test.pgm")
-except Exception as e:
-    print(type(e), e)
+    import os.path
+    this_folder = os.path.dirname(__file__)
+except ImportError:
+    # If we are missing os.path module, assume test file
+    # is in current working dir instead..
+    this_folder = "./"
 
-try:
-    ujpeg.decode_file("test.jpg", "/")
-except Exception as e:
-    print(type(e), e)
 
-# This will currently abort the program!
-# We should create a new error handler for the jpeg decoder,
-# now we use the default one that will exit() on pretty much
-# any error in the decoding..
-try:
-    ujpeg.decode_file("test.pgm", "test.pgm")
-except Exception as e:
-    print(type(e), e)
+if __name__ == "__main__":
+    jpeg_file = this_folder + "/test.jpg"
 
-# We never get here..
+    print("Decode file to pgm...")
+    try:
+        ujpeg.decode_file(jpeg_file, "test.pgm")
+        print("OK!")
+    except Exception:
+        print("Failed!")
+        raise
+
+    print("Try decode non existing file...")
+    try:
+        ujpeg.decode_file("/does-not-exist.jpg", "test.pgm")
+        print("OK!")
+    except Exception as e:
+        print(type(e), e)
+
+    print("Try writing output to invalid path...")
+    try:
+        ujpeg.decode_file(jpeg_file, "/")
+        print("No exception!")
+    except Exception as e:
+        print(type(e), e)
+        print("OK!")
+
+    # This will currently abort the program!
+    # We should create a new error handler for the jpeg decoder,
+    # now we use the default one that will exit() on pretty much
+    # any error in the decoding..
+    print("Try to decode invalid jpeg file...")
+    try:
+        ujpeg.decode_file("test.pgm", "test.pgm")
+        print("No exception!")
+    except Exception as e:
+        print(type(e), e)
+        print("OK!")  # We never get here..
+
+    # We never get here..
